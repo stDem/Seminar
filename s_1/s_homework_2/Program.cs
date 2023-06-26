22bf53397360d418d2ae34d5d956a3e2e576281f
LEX <anst.dem@mail.ru>
2023-06-26 20:36:14 +0300

    finished homework

--- s_1/s_homework_2/Program.cs
+++ s_1/s_homework_2/Program.cs
@@ -1,13 +1,9 @@
-﻿// Задача 4: Напишите программу, которая принимает на вход три числа
-// и выдаёт максимальное из этих чисел.
-// Console.WriteLine("Введите первое число");
-// int num_1 = int.Parse(Console.ReadLine());
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа
+и выдаёт максимальное из этих чисел.
 
-// Console.WriteLine("Введите второе число");
-// int num_2 = int.Parse(Console.ReadLine());
 
-// Console.WriteLine("Введите третье число");
-// int num_3 = int.Parse(Console.ReadLine());
+(ПОСМОТРИТЕ ЕЩЕ КОД В КОММЕНТАРИЯХ, ПОЖАЛУЙСТА, ОН НЕ ЗАПУСКАЕТСЯ, ХОТЯ ВРОДЕ ПРАВИЛЬНО)
+
 
 Console.WriteLine("Введите три числа");
 
@@ -23,5 +19,23 @@ for (int j = 1; j < new_read.length; j++)
             max = new_read[j];
     }
 
-Console.WriteLine($"Максимальное число = {max}");
+Console.WriteLine($"Максимальное число = {max}");*/
+
+Console.Write("Введите первое число: ");
+int num_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num_2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int num_3 = int.Parse(Console.ReadLine());
+
+int max = num_1;
+
+if (num_2 > max)
+    max = num_2;
+if (num_3 > max)
+    max = num_3;
+
+Console.Write($"Максимальное число: {max}");
 
