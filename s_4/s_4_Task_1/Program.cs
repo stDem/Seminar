cfee82f961a002131b21186d4c86620ea548e924
stDem <61629062+stDem@users.noreply.github.com>
2023-07-03 23:20:35 +0300

    Added code with converting number in line.cs

--- s_4/s_4_Task_1/Program.cs
+++ s_4/s_4_Task_1/Program.cs
@@ -1,25 +1,5 @@
-﻿// Задача 26: Напишите программу, которая принимает на вход число 
+// Задача 26: Напишите программу, которая принимает на вход число 
 // и выдаёт количество цифр в числе.
-int Prompt(string message)
-{
-    Console.Write(message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
-}
-void GetCount (int num)
-{
-    int counter = 0;
-    while (num != 0 )
-        {
-            num /= 10;
-            counter++;
-        }
-    Console.WriteLine("Количество:" + counter);
-}
-int C = Prompt("Введите число: ");
-GetCount(C);
-
 // int Prompt(string message)
 // {
 //     Console.Write(message);
@@ -29,7 +9,30 @@ GetCount(C);
 // }
 // void GetCount (int num)
 // {
-//     String(num) = Math.Abs()
+//     int counter = 0;
+//     while (num != 0 )
+//         {
+//             num /= 10;
+//             counter++;
+//         }
+//     Console.WriteLine("Количество:" + counter);
 // }
+// int C = Prompt("Введите число: ");
+// GetCount(C);
 
+//РЕШЕНИЕ С ПЕРЕВОДОМ ЧИСЛА В СТРОКУ:
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+void GetCount (int num)
+{
+    string numStr = Math.Abs(num).ToString();
+    Console.Write("Количество цифр в числе: " + numStr.Length);
+}
 
+int num_1 = Prompt("Введите число: ");
+GetCount(num_1);
