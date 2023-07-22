fc3b0fafd68eedcfb6e8c0bb3a77be4aff9cde35
stDem <61629062+stDem@users.noreply.github.com>
2023-07-23 01:03:51 +0300

    finished Homework 3, 4

---
+++ s_8_Homework/s_8_Homework_1/s_8_Homework_3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+//которая будет находить произведение двух матриц.
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int[,] FillArray(int m_QuantRow, int n_QuantColumn)
+{
+    int[,] array = new int[m_QuantRow, n_QuantColumn];
+    for (int i = 0; i < m_QuantRow; i++)
+    {
+        for (int j = 0; j < n_QuantColumn; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,3} ", array[i, j]));
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GetMatrixMultiplication(int[,] array, int[,] array1)
+{
+    int[,] array3 = new int[array.GetLength(0), array1.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                array3[i, j] += array[i, k] * array1[k, j];
+            }
+        }
+    }
+    return array3;
+}
+int M = Prompt("Введите количество строк для первой и второй матриц: ");
+int N = Prompt("Введите количество столбцов для первой и второй матриц: ");
+int[,] array = FillArray(M, N);
+int[,] array1 = FillArray(M, N);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(array1);
+Console.WriteLine("Произведение двух матриц: ");
+PrintArray(GetMatrixMultiplication(array, array1));
---
+++ s_8_Homework/s_8_Homework_1/s_8_Homework_4/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int[,,] FillArray(int m_QuantRow, int n_QuantColumn, int b)
+{
+    int[,,] array = new int[m_QuantRow, n_QuantColumn, b];
+    for (int i = 0; i < m_QuantRow; i++)
+    {
+        for (int j = 0; j < n_QuantColumn; j++)
+        {
+            for (int k = 0; k < n_QuantColumn; k++)
+            {
+                array[i, j, k] = new Random().Next(0, 10);
+                Console.Write($" ({i},{j},{k}) = {array[i, j, k]} ");
+            }
+        }
+    }
+    return array;
+}
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k]);
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int M = Prompt("Введите количество 1: ");
+int N = Prompt("Введите количество 2: ");
+int B = Prompt("Введите количество 3: ");
+int[,,] array = FillArray(M, N, B);
+Console.WriteLine();
+PrintArray(array);
