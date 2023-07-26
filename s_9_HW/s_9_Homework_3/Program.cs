486fd6ece31f5653fbe74079c3dd6175ad324c91
stDem <61629062+stDem@users.noreply.github.com>
2023-07-27 02:47:37 +0300

    s_9_HW_3

---
+++ s_9_HW/s_9_Homework_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 3. Дана строка, содержащая только следующие символы:'(', ')', '{', '}', '[', ']'. 
+//Определите, является ли ваша строка допустимой.
+string Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    return value;
+}
+bool IsStringOk(string input)
+{
+    if (string.IsNullOrEmpty(input))
+        return false;
+
+    Stack<char> stack = new();
+
+    static bool Open(char current) => current is '{' or '(';
+    static bool Close(char current) => current is '}' or ')';
+    bool Prev(char current) => stack.TryPop(out char previous) && previous == current switch { '}' => '{', ')' => '(' };
+
+    foreach (char current in input)
+    {
+        if (Open(current))
+            stack.Push(current);
+        else if (Close(current) && !Prev(current))
+            return false;
+    }
+
+    return !stack.Any();
+}
+string input = Prompt("Введите строку скобок: ");
+Console.WriteLine();
+if (IsStringOk(input))
+    Console.WriteLine("Строка верная");
+else
+    Console.WriteLine("Строка неверная");
